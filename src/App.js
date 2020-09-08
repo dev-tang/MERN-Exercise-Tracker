818a2f0dc20e283ddf34e9c29299e2049f5c9fb5
dev-tang <chelsea.hy.tang@gmail.com>
2020-09-07 23:40:14 -0700

    Frontend: React JS in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/Navbar"
+import ExercisesList from "./components/Exercises-List"
+import EditExercise from "./components/Edit-Exercise"
+import CreateExercise from "./components/Create-Exercise"
+import CreateUser from "./components/Create-User"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
